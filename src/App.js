cb563459cd70f17965080006cd39b9e8e6250fcf
Katherine Jiang <katherine.jiang30@gmail.com>
2022-07-13 21:56:47 +0000

    added word and letter components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { Letters } from './Letters';
+import { Word } from './Word';
 
 function App(props) {
   const [guessedLetters, setGuessedLetters] = React.useState([]);
---
+++ src/Word.js
@@ -0,0 +1,19 @@
+const Word = (props) => {
+  const charDivs = [];
+  for (const [i, letter] of Object.entries(props.word)) {
+    let displayLetter = null;
+    if (props.guessedLetters.includes(letter)) {
+      displayLetter = letter;
+    }
+
+    charDivs.push(
+      <div key={i} className="letter-box">
+        {displayLetter}
+      </div>,
+    );
+  }
+
+  return <section className="word-container">{charDivs}</section>;
+};
+
+export default Word;
